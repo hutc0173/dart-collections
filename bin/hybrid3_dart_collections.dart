cd2e920fcc560162488072b7a06c21cd2868771c
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-09-17 16:13:06 -0400

    first commit

---
+++ bin/hybrid3_dart_collections.dart
@@ -0,0 +1,5 @@
+import 'package:hybrid3_dart_collections/hybrid3_dart_collections.dart' as hybrid3_dart_collections;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hybrid3_dart_collections.calculate()}!');
+}
---
+++ test/hybrid3_dart_collections_test.dart
@@ -0,0 +1,8 @@
+import 'package:hybrid3_dart_collections/hybrid3_dart_collections.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

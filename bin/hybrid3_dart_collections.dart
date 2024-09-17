5e29495d5c7d3232b0803bcf0a0805f4c3f3876f
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-09-17 16:32:55 -0400

    sort method added

--- bin/hybrid3_dart_collections.dart
+++ bin/hybrid3_dart_collections.dart
@@ -1,4 +1,22 @@
-import 'package:hybrid3_dart_collections/hybrid3_dart_collections.dart' as hybrid3_dart_collections;
+import 'dart:convert';
+import 'package:hybrid3_dart_collections/hybrid3_dart_collections.dart'
+    as hybrid3_dart_collections;
+
+class Students {
+  List<Map<String, String>> people;
+
+  Students(this.people);
+
+  void sort(String field) {
+    people.sort((a, b) => a[field]!.compareTo(b[field]!))
+  }
+
+  void output() {
+    for (var person in people) {
+      print(person);
+    }
+  }
+}
 
 void main(List<String> arguments) {
   print('Hello world: ${hybrid3_dart_collections.calculate()}!');

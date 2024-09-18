b0ec93ed69a1baddd952d562b1a4fa7b87573de1
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-09-18 07:36:32 -0400

    methods added, need to finished main function

--- bin/hybrid3_dart_collections.dart
+++ bin/hybrid3_dart_collections.dart
@@ -8,7 +8,7 @@ class Students {
   Students(this.people);
 
   void sort(String field) {
-    people.sort((a, b) => a[field]!.compareTo(b[field]!))
+    people.sort((a, b) => a[field]!.compareTo(b[field]!));
   }
 
   void output() {
@@ -16,8 +16,32 @@ class Students {
       print(person);
     }
   }
+
+  void plus(Map<String, String> person) {
+    people.add(person);
+  }
+
+  void remove(String field) {
+    people.removeWhere((person) => person.values.contains(field));
+  }
 }
 
-void main(List<String> arguments) {
-  print('Hello world: ${hybrid3_dart_collections.calculate()}!');
+void main() {
+  String json = '''
+  [
+    {"first":"Steve", "last":"Griffith", "email":"griffis@algonquincollege.com"},
+    {"first":"Adesh", "last":"Shah", "email":"shaha@algonquincollege.com"},
+    {"first":"Tony", "last":"Davidson", "email":"davidst@algonquincollege.com"},
+    {"first":"Adam", "last":"Robillard", "email":"robilla@algonquincollege.com"}
+  ]
+  ''';
+
+  List<Map<String, String>> peopleList =
+      List<Map<String, String>>.from(jsonDecode(json));
+
+  Students students = Students(peopleList);
+
+  print("List of students");
+  students.output()
+
 }
